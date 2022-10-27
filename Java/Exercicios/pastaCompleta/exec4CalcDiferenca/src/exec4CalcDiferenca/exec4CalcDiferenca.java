d519cdaed6199ab638e74a0e99dafa4649ade46e
Willian Lima <willian.slima@mercadolivre.com>
2022-10-27 16:21:53 -0300

    Alterando organizacao de pastas

---
+++ Java/Exercicios/pastaCompleta/exec4CalcDiferenca/src/exec4CalcDiferenca/exec4CalcDiferenca.java
@@ -0,0 +1,32 @@
+package exec4CalcDiferenca;
+
+import java.util.Scanner;
+
+public class exec4CalcDiferenca {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// criando "methodos" para ler o que usuario escreve
+		Scanner leia = new Scanner(System.in);
+
+		// Variaveis
+
+		int numero1, numero2, numero3, numero4, diferenca;
+
+		System.out.println("Vamos calcular a diferença entre os numeros : ");
+		System.out.println("Digite o primeiro numero: "); // mensagem na tela
+		numero1 = leia.nextInt(); // Armazenar o que usuario escreveu
+		System.out.println("Digite o segundo numero: "); // mensagem na tela
+		numero2 = leia.nextInt(); // Armazenar o que usuario escreveu
+		System.out.println("Digite o terceiro numero: "); // mensagem na tela
+		numero3 = leia.nextInt(); // Armazenar o que usuario escreveu
+		System.out.println("Digite o quarto numero: "); // mensagem na tela
+		numero4 = leia.nextInt(); // Armazenar o que usuario escreveu
+
+		diferenca = (numero1 * numero2) - (numero3 * numero4);
+		System.out.println("A diferenca entre os numeros é: " + diferenca); // mensagem na tela
+
+	}
+
+}
