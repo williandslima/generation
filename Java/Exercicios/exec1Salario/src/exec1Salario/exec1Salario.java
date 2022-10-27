0f3c53b2354e522fb650c007436ecdcc07824a48
Willian Lima <willian.slima@mercadolivre.com>
2022-10-27 15:59:38 -0300

    Atualizando indent dos arquivos

--- Java/Exercicios/exec1Salario/src/exec1Salario/exec1Salario.java
+++ Java/Exercicios/exec1Salario/src/exec1Salario/exec1Salario.java
@@ -7,36 +7,32 @@ public class exec1Salario {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-				//criando "methodos"
-				Scanner leia = new Scanner(System.in);
-				
-				
-				//criando variaveis
-				float salario, abono, nSalario;
-				
-				
-				//digite o salario
-				System.out.println("Vamos calcular o Abono \n");
-				System.out.println("Digite o Salario: \n");
-				
-				//leia o salario
-				salario = leia.nextFloat();
-				
-				//digite o abono
-				System.out.println("Digite o Abono: ");
-				
-				//leia o abono
-				abono = leia.nextFloat();
-				
-				
-				//novo salario = salario + abono
-				nSalario = (salario + abono);
-				
-				// exibe novo salario
-				
-				System.out.println("\n O Novo salario e : " + nSalario);
-		
-		
+		// criando "methodos"
+		Scanner leia = new Scanner(System.in);
+
+		// criando variaveis
+		float salario, abono, nSalario;
+
+		// digite o salario
+		System.out.println("Vamos calcular o Abono \n");
+		System.out.println("Digite o Salario: \n");
+
+		// leia o salario
+		salario = leia.nextFloat();
+
+		// digite o abono
+		System.out.println("Digite o Abono: ");
+
+		// leia o abono
+		abono = leia.nextFloat();
+
+		// novo salario = salario + abono
+		nSalario = (salario + abono);
+
+		// exibe novo salario
+
+		System.out.println("\n O Novo salario e : " + nSalario);
+
 	}
 
 }
