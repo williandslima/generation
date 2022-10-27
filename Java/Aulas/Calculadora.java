9343bc3b491096afe66652a10afed6a75eabbbe5
Willian Lima <willian.slima@mercadolivre.com>
2022-10-27 17:29:47 -0300

    Atualizando repositorios

--- Java/Aulas/Calculadora.java
+++ Java/Aulas/Calculadora.java
@@ -4,36 +4,36 @@ import java.util.Scanner;
 public class Calculadora {
 
 	public static void main(String[] args) {
+// TODO Auto-generated method stub
+
 		// TODO Auto-generated method stub
 
-		//receber coisas do console system.in
-		//system.in 
-		
+		// receber coisas do console system.in
+		// system.in
+
 		Scanner leia = new Scanner(System.in); // classe para pegar informacoes criamos objeto
-		
+
 		// formatando as casas decimais
-		DecimalFormat df = new DecimalFormat ("###.##");	
-		
-		//criando variavel
+		DecimalFormat df = new DecimalFormat("###.##");
+
+		// criando variavel
 		int numero1, numero2;
-		
+
 		// receber os dados via teclado, numero inteiro
 		System.out.println("\n Digite o numero primeiro numero");
 		numero1 = leia.nextInt();
 		System.out.println("\n Digite o numero segundo numero");
 		numero2 = leia.nextInt();
-		
-		System.out.println("\n " + numero1 + "+" + numero2 + " = " + (numero1 + numero2) );
-		System.out.println("\n " + numero1 + "-" + numero2 + " = " + (numero1 - numero2) );
-		System.out.println("\n " + numero1 + "/" + numero2 + " = " + (numero1 / numero2) );
-		System.out.println("\n " + numero1 + "*" + numero2 + " = " + (numero1 * numero2) );
-		
+
+		System.out.println("\n " + numero1 + "+" + numero2 + " = " + (numero1 + numero2));
+		System.out.println("\n " + numero1 + "-" + numero2 + " = " + (numero1 - numero2));
+		System.out.println("\n " + numero1 + "/" + numero2 + " = " + (numero1 / numero2));
+		System.out.println("\n " + numero1 + "*" + numero2 + " = " + (numero1 * numero2));
+
 		// potencia
 		System.out.println("\n " + numero1 + "^" + numero2 + " = " + Math.pow(numero1, numero2));
-		//raiz quadrada
+		// raiz quadrada
 		System.out.println("\n Raiz quadrada de " + numero1 + " = " + df.format(Math.sqrt(numero1)));
-		
-		
 
 	}
 
